70f1b9e83dc9197db26083733a82848c01dc0a87
kanghua <kanghua@didiglobal.com>
2023-05-31 14:37:26 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ vcs import < guide_dog.repos src
 cd src/rslidar_sdk_ros2/
 git submodule init
 git submodule update
-
+cd -
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
