8ab5e17012a191e29d53e1aac2ad2e2cb325db85
kanghua <kanghua@didiglobal.com>
2023-05-31 00:45:24 +0000

    update for guide_dog env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-vcs import < src/ros2.repos src
+vcs import < src/guide_dog.repos src
+cd src/rslidar_sdk_ros2/
+git submodule init
+git submodule update
+
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
