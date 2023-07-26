dd9d4471ba70a3a9851b8ca1ac69d113d449783b
king <kanghua151@msn.com>
2023-07-26 16:40:57 +0000

    test

--- setup.sh
+++ setup.sh
@@ -2,14 +2,10 @@
 set -e
 
 vcs import < guide_dog.repos src
-cd src/rslidar_sdk_ros2/
-git submodule init
-git submodule update
-cd -
 
 sudo apt-get update
 rosdepc update
-rosdepc install --from-paths src --ignore-src -y
+rosdepc install --from-paths src --ignore-src -r -y
 
 
 
