0299143e2a55db7d0bf8e2dc7897afe5714f1542
kanghua <kanghua@didiglobal.com>
2023-07-28 09:30:10 +0000

    xxxx

--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@ set -e
 vcs import < guide_dog.repos src
 
 sudo apt-get update
-rosdepc update
-rosdepc install --from-paths src --ignore-src -r -y
+rosdep init
+rosdep update --include-eol-distros #需要加这个才能把foxy的key index 引入，而rosdepc 不支持这个参数
+rosdep install --from-paths src --ignore-src -r -y
+
+#
+# 有可能需要设置 “185.199.108.133  raw.githubusercontent.com” 在 /etc/hosts -- 因为国内dns修改
+# 可查询ip地址 -- https://sites.ipaddress.com/raw.githubusercontent.com/
+#
 
 
 
