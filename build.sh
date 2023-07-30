576dea615ca3511f8c708d9a593768c18699d8bb
kanghua309 <kanghua151@msn.com>
2023-07-30 02:40:21 -0700

    for foxy ros2

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+#rm guide_dog_unitree_go1 listen_talk_ros2 demos -rf #这里要把src 外的package 删除了，不然报错
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
 colcon build \
+        --base-paths src \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
+
+mkdir -p src
+
 vcs import < guide_dog.repos src
 
 sudo apt-get update
-rosdep init
-rosdep update --include-eol-distros #需要加这个才能把foxy的key index 引入，而rosdepc 不支持这个参数
+rosdep update --include-eol-distros --rosdistro=foxy  #需要加这个才能把foxy的key index 引入，而rosdepc 不支持这个参数
 rosdep install --from-paths src --ignore-src -r -y
 
 #
-# 有可能需要设置 “185.199.108.133  raw.githubusercontent.com” 在 /etc/hosts -- 因为国内dns修改
+# 有可能需要设置 “185.199.108.133  raw.githubusercontent.com” 在 /etc/hosts -- 因为国内dns被污染
 # 可查询ip地址 -- https://sites.ipaddress.com/raw.githubusercontent.com/
 #
 
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
 
 
 
